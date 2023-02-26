097abdea1462fa9ea9728eb23eef5101fe0205e0
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-02-26 15:06:03 -0500

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+    
 # setup
 mkdir -p /usr/local/bin/
 cat<<'EOF' > /usr/local/bin/addTimeProfile
 #!/bin/bash
-
 {
-
     # set filename to save time profile at. if it already exists append its contents to "old" copy.
     [[ -n ${FUNCNAME[1]} ]] && timeProfileFile=".timeprofile.${FUNCNAME[1]}" || timeProfileFile=".timeprofile.${BASH_SOURCE[1]##*/}"
     [[ -f "${timeProfileFile}" ]] && cat "${timeProfileFile}"  >> "${timeProfileFile}.old"
@@ -17,38 +16,41 @@ cat<<'EOF' > /usr/local/bin/addTimeProfile
     # since the last command triggered the DEBUG trap
     # and print elapsed time + the command that was run
     { coproc pTime {
-
         # set initial values for "last" time/command
         ${haveEpochFlag} && { a="$EPOCHREALTIME"; d0=6; } || { a="$(date +'%s.%N')"; d0=9; }
         REPLY0='BEGIN FUNCTION'
-
         while true; do
             # read $BASH_COMMAND on stdin ("current" command) --> triggers getting "current" time --> get elapsed time
             read -r
-
             # get time difference between "last" and "current" time (in microseconds)
             ${haveEpochFlag} && b="$EPOCHREALTIME" || b="$(date +'%s.%N')"
             c=$(( ${b//./} -  ${a//./} ))
-
             # print elapsed time + last command
              d=$(( ${#c} - ${d0} ))
             (( $d > 0 )) && printf '%s seconds%s\n' "${c:0:${d}}.${c:${d}}" "${REPLY0}" >&4 || printf '0.%0.'${d0}'d seconds%s\n' "$c" "${REPLY0}" >&4
-
             # cycle "current" time/command to "last" time/command
             a=$b
             REPLY0="${REPLY}"
         done
         }
-
     # redirect to file. redirect to stderr instead by changing to `5>&2`, but warning: this may cause some scripts to not exit properly and get stuck in a loop.
     } 4>"${timeProfileFile}"
-
-    # set DEBUG trap to send BASH_COMMAND to coproc
-    trap 'echo ": ${BASH_COMMAND}" >&${pTime[1]} || :' DEBUG
-
-    # set exit trap to clear debug trap. If another exit trap already exists add this into it.
-    trap 'trap - DEBUG; echo "The generated line-by-line time profile is located at '"${timeProfileFile}"'" >&2' EXIT TERM HUP QUIT
     
+    # set DEBUG trap to send BASH_COMMAND to coproc       
+    # set EXIT trap to clear debug trap. 
+    # If another exit/debug trap already exists add these into it.
+    for nn in EXIT HUP TERM QUIT DEBUG; do 
+        curTrap="$(trap -p "$nn")"
+        curTrap="${curTrap#*"'"}"
+        curTrap="${curTrap%"'"*}"
+        [[ -n "${curTrap}" ]] && trapNew="${curTrap}"'; ' || trapNew=''
+        if [[ "$nn" == DEBUG ]]; then
+            trapNew="$trapNew"'echo ": ${BASH_COMMAND}" >&${pTime[1]} || :'              
+        else
+            trapNew="$trapNew"'trap - DEBUG; echo "The generated line-by-line time profile is  located at '"${timeProfileFile}"'" >&2' 
+        fi
+        trap -- "$trapNew" "$nn"
+    done        
 }
 EOF
 
