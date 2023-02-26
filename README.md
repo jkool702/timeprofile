448f6d446495b1e163ad3c41b3718abc5cf72a46
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-02-26 01:16:50 -0500

    complete re-write to make it *much* faster

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # timeprofile
-tool for measuring the execution time of all commands called by a shell script or shell function
+A tool for measuring the execution time of all commands called by a shell script or shell function very efficiently using a DEBUG trap and a bash coproc.
+
+USAGE: run `setup.bash` to save the required script to `/usr/local/bin/addTimeProfile`. To actually utalize this script and generate a timeprofile, add the following line to the top of the script (just after shebang) / function (just after function declarion) that you want to profile:
+
+    [[ -n ${timeProfileFlag} ]] && ${timeProfileFlag} && source /usr/local/bin/addTimeProfile
+
+Then, to generate a profile, run the function/script using:
+
+    timeProfileFlag=true <funcName>
+
+The timeprofile will be savedin your current directory under the name `.timeprofile.<funcName>`. If this file exists the old version will first be appended to `.timeprofile.<funcName>.old`.
+
+NOTE: the timeprofile is generated *much* more efficiently in bash 5+ due to the availability of `$EPOCHREALTIME`. Most bash scripts will run at >80\% of the speed they run without being profiled. Extreamly optimized shell scripts that deal with many very fast iterations may slow down by a factor of 2-3x in a worst-case scenario. 
+
+The timeprofile can still be generated using bash 4 (using `$(date +'%s.%N')`), but will cause significant slowing down of the function being profiled. Bash 3 and older are not supported due to not having coprocs. The `timeprofile/old/bash` script *might* work for bash 3 and older, but it is not supported noir tested.
---
+++ setup.bash
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# setup
+mkdir -p /usr/local/bin/
+cat<<'EOF' > /usr/local/bin/addTimeProfile
+#!/bin/bash
+
+{
+
+    # set filename to save time profile at. if it already exists append its contents to "old" copy.
+    [[ -n ${FUNCNAME[1]} ]] && timeProfileFile=".timeprofile.${FUNCNAME[1]}" || timeProfileFile=".timeprofile.${BASH_SOURCE[1]##*/}"
+    [[ -f "${timeProfileFile}" ]] && cat "${timeProfileFile}"  >> "${timeProfileFile}.old"
+    
+    (( $(bash --version | head -n 1 | grep -oE 'version [0-9]+' | cut -d ' ' -f 2) >= 5 )) && haveEpochFlag=true || haveEpochFlag=false
+    
+    # fork a coproc that will determine elapsed time
+    # since the last command triggered the DEBUG trap
+    # and print elapsed time + the command that was run
+    { coproc pTime {
+
+        # set initial values for "last" time/command
+        ${haveEpochFlag} && { a="$EPOCHREALTIME"; d0=6; } || { a="$(date +'%s.%N')"; d0=9; }
+        REPLY0='BEGIN FUNCTION'
+
+        while true; do
+            # read $BASH_COMMAND on stdin ("current" command) --> triggers getting "current" time --> get elapsed time
+            read -r
+
+            # get time difference between "last" and "current" time (in microseconds)
+            ${haveEpochFlag} && b="$EPOCHREALTIME" || b="$(date +'%s.%N')"
+            c=$(( ${b//./} -  ${a//./} ))
+
+            # print elapsed time + last command
+             d=$(( ${#c} - ${d0} ))
+            (( $d > 0 )) && printf '%s seconds%s\n' "${c:0:${d}}.${c:${d}}" "${REPLY0}" >&4 || printf '0.%0.'${d0}'d seconds%s\n' "$c" "${REPLY0}" >&4
+
+            # cycle "current" time/command to "last" time/command
+            a=$b
+            REPLY0="${REPLY}"
+        done
+        }
+
+    # redirect to file. redirect to stderr instead by changing to `5>&2`, but warning: this may cause some scripts to not exit properly and get stuck in a loop.
+    } 4>"${timeProfileFile}"
+
+    # set DEBUG trap to send BASH_COMMAND to coproc
+    trap 'echo ": ${BASH_COMMAND}" >&${pTime[1]} || :' DEBUG
+
+    # set exit trap to clear debug trap. If another exit trap already exists add this into it.
+    trap 'trap - DEBUG; echo "The generated line-by-line time profile is located at '"${timeProfileFile}"'" >&2' EXIT TERM HUP QUIT
+    
+}
+EOF
+
+printf '%s\n' '' 'TO GENERATE A TIME PROFILE FOR A FUNCTION/SCRIPT, ADD THE FOLLOWING LINE TO THE TOP OF THE FUNCTION/SCRIPT:' '' '    [[ -n ${timeProfileFlag} ]] && ${timeProfileFlag} && source /usr/local/bin/addTimeProfile' '' 'THEN, CALL THE FUNCTION/SCRIPT VIA:' '' '    timeProfileFlag=true <funcName>' ''
