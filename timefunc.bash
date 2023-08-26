d27c895f464c8bd6002f81f97cf888c27161d2df
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-08-26 18:37:20 -0400

    Update timefunc.bash

--- timefunc.bash
+++ timefunc.bash
@@ -1,33 +1,47 @@
 timefunc() {
-## Generates a line-by-line execution time profile for a bash function with very minimal overhead
+## Generates a line-by-line execution time profile for a bash function / script with very minimal overhead
 # The 'time profile' is printed to stderr and includes (for each line): 
-# total cumulative execution time, line number, command run, and number of times the command was run
+# total cummulative execution time, line number, command run, and number of times the command was run
 #
 # NOTE: Bash 5+ is required due to the use of the ${EPOCHREALTIME} builtin variable.
 #
+#
 # # # # # USAGE # # # # #
+#
 # func() { ... }; timefunc func func_args
 # timefunc (-s|--src|--source) <src> func func_args
-# timefunc (-s|--src|--source)=<src> func func_args
+# timefunc (--src|--source)=<src> func func_args
 # { ... } | timefunc [(-s|--src|--source) <src>] func funcArgs
+# timefunc (-S|--script) <scriptPath> script_args
+# timefunc (-S|--script)=<scriptPath> script_args
+# { ... } | timefunc (-S|--script) <scriptPath> script_args
+#
 #
 # # # # # OPTIONS # # # # #
+#
 # The bash fuction being profiled may either be sourced prior to running `timefunc`, or 
-# it can be sourced by specifying its location (which will be sourced) as timefunc's 1st option via any of:
+# it can be sourced by specifying its location (which will be sourced) as timefunc's 1st option(s) via any of:
 #    -s <src> ; --src <src> ; --src=<src> ; --source <src>; --source=<src>
 #
 # NOTE: you can source non-file-paths by setting <src> as a single-quoted '<( ... )' command. example:
 #    timefunc -s '<(curl '"'"'https://raw.githubusercontent.com/jkool702/timeprofile/main/timefunc.bash'"'"')'
 #
+# Though originally intended for just functions, timefunc was extended to generate time profiles for bash scripts
+# This is done by wrapping the script inside of a dummy function. To use, set timefunc's 1st option(s) as any of:
+#    -S <scrptPath> ; --script <scriptPath> ; --script=<scriptPath>
+#
 # IFF timefunc's STDIN is a pipe ({ ... } | timefunc <...>), it will be passed to func's STDIN when it is called
 #
-################################################################################################################
+#################################################################################################################
 
 (
 local -ai tDiffA0 tDiffA1
 local -a tCmdA fSrc
 local -i tFinal0 tFinal1
-local t0 tStart tStop PREV_CMD PREV_LINENO srcPath tStart tStop tFinal0 tFinal1
+local t0 tStart tStop PREV_CMD PREV_LINENO srcPath tStart tStop tFinal0 tFinal1 scriptFlag verboseFlag last_subshell
+
+# if true; trigger exit trap on every subshell exit 
+verboseFlag=true
 
 shopt -s extglob
 set -T
@@ -36,12 +50,16 @@ tic() {
     ## start the timer
     t0=${EPOCHREALTIME}; 
     tStart="${t0}"
+    last_subshell=0
 }
 
 toc() { 
     ## record the command run and the time taken by command, then restart timer
     # save/update at array element array[$LINENO]
     # note: the time recorded this iteration is for the previous iteration's command
+    
+    # set exit trap from debug trap so that `set -T` propogates it too
+    [[ "${last_subshell}" == "${3}" ]] || { last_subshell="${3}"; trap 'trap - DEBUG; fExit >&2; : timefunc_exitTrapSet' EXIT INT TERM HUP QUIT; }
     
     # dont include commands from timefunc
     [[ ${#BASH_SOURCE[@]} == 1 ]] && return
@@ -81,7 +99,7 @@ fExit() {
     
         [[ $kk == 1 ]] && continue
     
-        # make sure cumulative microsecond count is between 0 and 0.999999 seconds
+        # make sure cummulative microsecond count is between 0 and 0.999999 seconds
         while (( ${tDiffA1[$kk]} < 0 )); do
             ((tDiffA0[$kk]--))
             tDiffA1[$kk]=$(( ${tDiffA1[$kk]} + 1000000 ))
@@ -102,26 +120,57 @@ fExit() {
         tCmdA[$kk]="$(echo "${tCmdA[$kk]}" | sort | uniq -c | tail -n +2 | sed -E s/'^[ \t]*([0-9]+) '/'(\1x) '/)"
         
         # print line for final generated time profile
-        printf '%d:\t%d.%06d sec \t{  %s}\n' "${kk}" "${tDiffA0[$kk]}" "${tDiffA1[$kk]}" "$(IFS=$'\n'; printf '%s;  ' ${tCmdA[$kk]})"
+        { ${verboseFlag} || [[ "${last_subshell}" == 1 ]]; } && printf '%d.%d: %d.%06d sec \t{  %s}\n' "${last_subshell}" "${kk}" "${tDiffA0[$kk]}" "${tDiffA1[$kk]}" "$(IFS=$'\n'; printf '%s;  ' ${tCmdA[$kk]})"
     done
     
     # print total execution time
-    tFinal0=$(( ${tStop%.*} - ${tStart%.*} )) 
-    tFinal1=$(( ${tStop##*.*(0)} - ${tStart##*.*(0)} ))
-    (( ${tFinal1} < 0 )) && { ((tFinal0--)); tFinal1=$(( ${tFinal1} + 1000000 )); }
-    printf '\nTOTAL TIME TAKEN: %d.%06d seconds\n\n' ${tFinal0} ${tFinal1}
+   { ${verboseFlag} || [[ "${last_subshell}" == 1 ]]; } && {
+        tFinal0=$(( ${tStop%.*} - ${tStart%.*} )) 
+        tFinal1=$(( ${tStop##*.*(0)} - ${tStart##*.*(0)} ))
+        (( ${tFinal1} < 0 )) && { ((tFinal0--)); tFinal1=$(( ${tFinal1} + 1000000 )); }
+        printf '\nTOTAL TIME TAKEN: %d.%06d seconds\n\n' ${tFinal0} ${tFinal1}
+    }
 }
 
 # if 1st timefunc option specifies what to source to get the function, parse it and source it
-if [[ ${1,,} =~ ^-+s((ource)|(rc))?$ ]]; then
+scriptFlag=false
+if [[ ${1} =~ ^-+((S)|(script))?$ ]]; then
+    srcPath="${2}"
+    scriptFlag=true
+    shift 2
+elif  [[ ${1} =~ ^-+((S)|(script))=.+$ ]]; then
+    srcPath="${1#*=}"
+    scriptFlag=true
+    shift 1
+elif [[ ${1} =~ ^-+s((ource)|(rc))?$ ]]; then
     srcPath="${2}"
     shift 2
-elif  [[ ${1,,} =~ ^-+s((ource)|(rc))?=.+$ ]]; then
+elif  [[ ${1} =~ ^-+s((ource)|(rc))=.+$ ]]; then
     srcPath="${1#*=}"
     shift 1
 fi
 if [[ -n "${srcPath}" ]]; then
-    if [[ -f "${srcPath}" ]]; then
+    if ${scriptFlag}; then
+        if [[ -f "${srcPath}" ]]; then
+            if [[ "$(cat "${srcPath}" | head -n 1)" == '#!'* ]]; then
+                source <(cat<<EOF
+$(cat "${srcPath}" | head -n 1)
+tfunc() {
+$(cat "${srcPath}" | tail -n +2)
+}
+EOF
+)
+            else
+                source <(cat<<EOF
+tfunc() {
+$(cat "${srcPath}")
+}
+EOF
+)
+            fi
+            [[ "${1}" == "${srcPath}" ]] && shift 1
+        fi
+    elif [[ -f "${srcPath}" ]]; then
         source "${srcPath}"
     elif [[ "${srcPath}" == '<('* ]]; then
         source <(cat<<EOF
@@ -133,27 +182,28 @@ EOF
     fi
 fi
 
-# check that the function is sourced
-declare -F "${1}" || { printf '\nERROR: SPECIFIED FUNCTION (%s) NOT SOURCED. PLEASE SOURCE IT AND RE-RUN.\n\n' "${1}" >&2; return 1; }
+${scriptFlag} && fName='tfunc' || fName="${1}"
 
-# add in a few dummy commands (` :; `) a few places to ensure the DEBUG trap captures the command
-mapfile -t fSrc < <(declare -f "${1}" | sed -E s/'^((.*;)?[ \t]*)?((for)|(while)|(until)|(if)|(elif))(([ \t;])|$)'/'\1 :; \3 '/g)
+# check that the function is sourced
+declare -F "${fName}" || { printf '\nERROR: SPECIFIED FUNCTION (%s) NOT SOURCED. PLEASE SOURCE IT AND RE-RUN.\n\n' "${1}" >&2; return 1; }
+
+# add in a few dummy commands (` :; `) a few places to ensuyre the DEBUG trap captures the command
+mapfile -t fSrc < <(declare -f "${fName}" | sed -E s/'^((.*;)?[ \t]*)?((for)|(while)|(until)|(if)|(elif))(([ \t;])|$)'/'\1 :; \3 '/g)
 source <(printf '%s\n' "${fSrc[@]:0:2}" ':;' "${fSrc[@]:2}")
 
-# set traps and start timer
-trap 'trap - DEBUG; fExit >&2' EXIT INT TERM HUP QUIT
+# start timer
 tic
+
+# set traps
+trap 'toc "${LINENO}" "${BASH_COMMAND}" "${BASH_SUBSHELL}"' DEBUG
 
 # run function (passing it STDIN if needed)
 if [[ -t 0 ]]; then
-    trap 'toc "${LINENO}" "${BASH_COMMAND}"' DEBUG
-    "${@}"
+    ${scriptFlag} && tfunc "${@}" || "${@}"
 else
-    trap 'toc "${LINENO}" "${BASH_COMMAND}"' DEBUG
-    "${@}" <&0
+    ${scriptFlag} && tfunc "${@}" <&0 || "${@}" <&0
 fi
 trap - DEBUG
-tStop="${EPOCHREALTIME}"
 
 ) 
 } 
