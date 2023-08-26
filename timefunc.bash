b610c4d28d46e3c7d33cb0a380fdcb4c8a71a3e0
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-08-26 19:38:19 -0400

    Update timefunc.bash

--- timefunc.bash
+++ timefunc.bash
@@ -1,25 +1,28 @@
 timefunc() {
 ## Generates a line-by-line execution time profile for a bash function / script with very minimal overhead
 # The 'time profile' is printed to stderr and includes (for each line): 
-# total cummulative execution time, line number, command run, and number of times the command was run
+# total cumulative execution time, line number, command run, and number of times the command was run
 #
 # NOTE: Bash 5+ is required due to the use of the ${EPOCHREALTIME} builtin variable.
 #
 #
 # # # # # USAGE # # # # #
 #
-# func() { ... }; timefunc func func_args
-# timefunc (-s|--src|--source) <src> func func_args
-# timefunc (--src|--source)=<src> func func_args
-# { ... } | timefunc [(-s|--src|--source) <src>] func funcArgs
-# timefunc (-S|--script) <scriptPath> script_args
-# timefunc (-S|--script)=<scriptPath> script_args
-# { ... } | timefunc (-S|--script) <scriptPath> script_args
+# source /path/to/timefunc.bash
+# source <(curl 'https://raw.githubusercontent.com/jkool702/timeprofile/main/timefunc.bash')
+#
+# func() { ... }; timefunc func [func_args]
+# timefunc (-s|--src|--source) <src> func [func_args]
+# timefunc (-s|--src|--source)=<src> func [func_args]
+# { ... } | timefunc [(-s|--src|--source) <src>] func [func_args]
+# timefunc (-S|--script) <scriptPath> [script_args]
+# timefunc (-S|--script)=<scriptPath> [script_args]
+# { ... } | timefunc (-S|--script) <scriptPath> [script_args]
 #
 #
 # # # # # OPTIONS # # # # #
 #
-# The bash fuction being profiled may either be sourced prior to running `timefunc`, or 
+# The bash function being profiled may either be sourced prior to running `timefunc`, or 
 # it can be sourced by specifying its location (which will be sourced) as timefunc's 1st option(s) via any of:
 #    -s <src> ; --src <src> ; --src=<src> ; --source <src>; --source=<src>
 #
@@ -27,8 +30,8 @@ timefunc() {
 #    timefunc -s '<(curl '"'"'https://raw.githubusercontent.com/jkool702/timeprofile/main/timefunc.bash'"'"')'
 #
 # Though originally intended for just functions, timefunc was extended to generate time profiles for bash scripts
-# This is done by wrapping the script inside of a dummy function. To use, set timefunc's 1st option(s) as any of:
-#    -S <scrptPath> ; --script <scriptPath> ; --script=<scriptPath>
+# This is done by wrapping the script inside of a dummy function. To use, set timefunc's 1st option(s) to any of:
+#    -S <scriptPath> ; --script <scriptPath> ; --script=<scriptPath>
 #
 # IFF timefunc's STDIN is a pipe ({ ... } | timefunc <...>), it will be passed to func's STDIN when it is called
 #
@@ -58,7 +61,7 @@ toc() {
     # save/update at array element array[$LINENO]
     # note: the time recorded this iteration is for the previous iteration's command
     
-    # set exit trap from debug trap so that `set -T` propogates it too
+    # set exit trap from debug trap so that `set -T` propagates it too
     [[ "${last_subshell}" == "${3}" ]] || { last_subshell="${3}"; trap 'trap - DEBUG; fExit >&2; : timefunc_exitTrapSet' EXIT INT TERM HUP QUIT; }
     
     # dont include commands from timefunc
@@ -69,7 +72,7 @@ toc() {
     t1=${EPOCHREALTIME}; 
 
     [[ -n ${PREV_LINENO} ]] && {
-        # add time taken to count. add seconds and microseconds seperately.
+        # add time taken to count. add seconds and microseconds separately.
         [[ -n ${tDiffA0[${PREV_LINENO}]} ]] || tDiffA0[${PREV_LINENO}]=0
         [[ -n ${tDiffA1[${PREV_LINENO}]} ]] || tDiffA1[${PREV_LINENO}]=0
         tDiffA0[${PREV_LINENO}]+=$(( ${t1%.*} - ${t0%.*} ))
@@ -99,7 +102,7 @@ fExit() {
     
         [[ $kk == 1 ]] && continue
     
-        # make sure cummulative microsecond count is between 0 and 0.999999 seconds
+        # make sure the cumulative microsecond count is between 0 and 0.999999 seconds
         while (( ${tDiffA1[$kk]} < 0 )); do
             ((tDiffA0[$kk]--))
             tDiffA1[$kk]=$(( ${tDiffA1[$kk]} + 1000000 ))
@@ -187,7 +190,7 @@ ${scriptFlag} && fName='tfunc' || fName="${1}"
 # check that the function is sourced
 declare -F "${fName}" || { printf '\nERROR: SPECIFIED FUNCTION (%s) NOT SOURCED. PLEASE SOURCE IT AND RE-RUN.\n\n' "${1}" >&2; return 1; }
 
-# add in a few dummy commands (` :; `) a few places to ensuyre the DEBUG trap captures the command
+# add in a few dummy commands (` :; `) a few places (e,.g. just before loops) to ensure the DEBUG trap captures the command properly
 mapfile -t fSrc < <(declare -f "${fName}" | sed -E s/'^((.*;)?[ \t]*)?((for)|(while)|(until)|(if)|(elif))(([ \t;])|$)'/'\1 :; \3 '/g)
 source <(printf '%s\n' "${fSrc[@]:0:2}" ':;' "${fSrc[@]:2}")
 
